e595b8272190d3a4d18b8d54b808183c4a374dd4
illiamartynov <ilyamartynov989@gmail.com>
2024-03-10 01:45:31 +0100

    no votes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var endDate = new Date("March 9, 2024 22:00:00").getTime();
+    var endDate = new Date("March 9, 2025 22:00:00").getTime();
     var timer = document.getElementById("timer");
     var voteBtn = document.getElementById("voteBtn");
     var vote = document.getElementsByClassName("vote");
